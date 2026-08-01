a493feddec4a4cbff85ee1fea8d0be7f056566a2
Parisa Emamy <Parisa2emami@ymail.com>
2026-08-01 10:56:45 +0330

    Create create_recom.py

---
+++ Weather-Morning-assistance/create_recom.py
@@ -0,0 +1,25 @@
+from data_prepration import weather
+
+def make_recom(w):
+    msg=""
+    total_txt=["Today's Weather:"]
+    total_txt.append(w["W_C"])
+    if w["Prob_p"]>=60:
+        msg="Take umbrella, raincoat."
+    elif 40<w["Prob_p"]<60 :
+        msg="probably you might need an umbrella."
+    total_txt.append(msg)
+    t=int(w["T"])
+    total_txt.append(f"Temperature_ave:{t}")
+    if w["T"]<10:
+        msg="wear a warm jacket."
+    if 10<=w["T"]<18:
+        msg="take a light jacket."
+    if 18<=w["T"]<28:
+        msg="comfortable clothes is recommended."
+    if w["T"]>28:
+        msg="wear light clothes and stay hydrated."
+    total_txt.append(msg)
+    return "\n".join(total_txt)
+
+message=make_recom(weather)
