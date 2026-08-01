04a5ca524ff08ba638a2cc6a268293f69ecd6ef4
Parisa Emamy <Parisa2emami@ymail.com>
2026-08-01 11:08:09 +0330

    Create WMO_code.py

---
+++ Weather-Morning-assistance/WMO_code.py
@@ -0,0 +1,40 @@
+WEATHER_CODES = {
+    0: "☀️ Clear sky",
+    1: "🌤 Mainly clear",
+    2: "⛅ Partly cloudy",
+    3: "☁️ Overcast",
+
+    45: "🌫 Fog",
+    48: "🌫 Depositing rime fog",
+
+    51: "🌦 Light drizzle",
+    53: "🌦 Moderate drizzle",
+    55: "🌧 Dense drizzle",
+
+    56: "🌧 Freezing drizzle (Light)",
+    57: "🌧 Freezing drizzle (Dense)",
+
+    61: "🌦 Slight rain",
+    63: "🌧 Moderate rain",
+    65: "🌧 Heavy rain",
+
+    66: "🌧 Freezing rain (Light)",
+    67: "🌧 Freezing rain (Heavy)",
+
+    71: "❄️ Slight snowfall",
+    73: "❄️ Moderate snowfall",
+    75: "❄️ Heavy snowfall",
+    77: "🌨 Snow grains",
+
+    80: "🌦 Slight rain showers",
+    81: "🌧 Moderate rain showers",
+    82: "⛈ Violent rain showers",
+
+    85: "🌨 Slight snow showers",
+    86: "❄️ Heavy snow showers",
+
+    95: "⛈ Thunderstorm",
+
+    96: "⛈ Thunderstorm with slight hail",
+    99: "⛈ Thunderstorm with heavy hail"
+}
